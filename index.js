afed5c48b4d098bc25a8e2d67e5c99382d92472b
yue shan <fuhuodemao@163.com>
2019-12-14 17:01:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,288 +1,313 @@
-(function (win, dom) {
-  var defaultOptions = {
-    content: null, //容器id
-    contentElement: null, //容器元素
-    data: [], //所有的数据
-    random: [], //所有的字段
-    range: [1, 5], //最小行/最大行
-    nodes: [], //生成的节点
-    rule: function () {
-    }
-  };
+(function(win, dom) {
+	var defaultOptions = {}
 
-  function createDewChart(options) {
-    var ops = Object.assign({}, defaultOptions, options);
+	function createDewChart(options) {
+		defaultOptions = {
+			content: null, // 容器id
+			contentElement: null, // 容器元素
+			data: [], // 所有的数据
+			random: [], // 所有的字段
+			range: [1, 5], // 最小行/最大行
+			nodes: [], // 生成的节点
+			positions: [], // 节点定位
+			list: 1,
+			baseWidth: 30,
+			style: 'solid', // solid / hollow
+			rule: function() {}
+		}
 
-    ops.contentElement = _renderContent(ops); //渲染容器
-    _renderColLine(ops); //渲染列
-    _renderRowLine(ops); //渲染行
-    ops.random = _getRandom(ops.data); //获取随机值列表
-    ops.nodes = _createNode(ops); //将data转化为node
-    _renderNode(ops); //将nodes渲染程dom的真实node
-  }
+		var ops = Object.assign({}, defaultOptions, options, {
+			positions: [],
+			nodes: [],
+			list: 1
+		})
 
-  function _getRandom(data) {
-    var obj = {};
-    var length = data.length;
-    for (var i = 0; i < length; i++) {
-      if (!obj.hasOwnProperty(data[i])) {
-        obj[data[i]] = 0;
-      }
-      obj[data[i]]++;
-    }
-    return Object.getOwnPropertyNames(obj);
-  }
+		ops.contentElement = _renderContent(ops) // 渲染容器
+		ops.random = _getRandom(ops.data) // 获取随机值列表
+		ops.nodes = _createNode(ops) // 将data转化为node
+		_renderColLine(ops) // 渲染列
+		_renderRowLine(ops) // 渲染行
+		_renderNode(ops) // 将nodes渲染程dom的真实node
+	}
 
-  function _createNode(ops) {
-    var nodes = [];
-    var data = ops.data;
-    var range = ops.range;
-    var min = range[0];
-    var max = range[1];
-    var obj = {direction: "down"};
-    var length = data.length;
-    var prev_index = 0;
-    var curr_index = 0;
-    var i, node, current;
-    var bool = true;
+	function _getRandom(data) {
+		var obj = {}
+		var length = data.length
+		for (var i = 0; i < length; i++) {
+			if (!obj.hasOwnProperty(data[i])) {
+				obj[data[i]] = 0
+			}
+			obj[data[i]]++
+		}
+		return Object.getOwnPropertyNames(obj)
+	}
 
-    // obj : { direction: "down" , min:1 , max:5 , 龙:9}
-    for (i = 0; i < length; i++) {
-      if (i === 0) {
-        current = data[i]; //大,小,小,大
-        obj[data[i]] = 1;
-      } else {
-        //如果current不等于循环值,且obj中不存在循环值属性,需要设置新的obj
-        if (current !== data[i] && !obj[data[i]]) {
-          if (obj[current] > range[1]) {
-            prev_index = nodes.length;
-          }
-          obj.min = min;
-          obj.max = max;
-          nodes.push(obj);
-          obj = {direction: "down"};
-          current = data[i];
-          obj[data[i]] = 1;
-          max = range[1];
-          min = range[0];
-          bool = true;
-        } else {
-          //在min和max的范围之内,当前属性值正常往下加
-          if (obj[data[i]] >= min && obj[data[i]] < max) {
-            obj[data[i]]++;
-          } else {
-            //当统计总数超多max的最大限制之后,需要将方向设置为右边,同时继续累积当前的统计数
-            curr_index = nodes.length;
-            obj[data[i]]++;
-            obj["direction"] = "right";
+	function DewNode(name, pos, direction) {
+		this.name = name
+		this.row = pos[0] // 行
+		this.col = pos[1] // 列
+		this.position = [this.row, this.col]
+		this.list = defaultOptions.list
+		this.direction = direction // forward , right
+	}
 
-            if (prev_index !== 0) {
-              node = nodes.slice(prev_index, prev_index + 1)[0];
-              if (node) {
-                //当前累积值与最大值的差大于两个节点之间的index差的时候,继续累加会顶到之前的节点,需要减少当前节点的最大值
-                var key = intersection(Object.getOwnPropertyNames(node), ops.random);
-                if ((node[key] - node["max"]) > (curr_index - prev_index)) {
-                  bool && max--;
-                  bool = false;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+	DewNode.prototype.getCol = function(minus) {
+		if (minus) {
+			return this.col + minus
+		}
+		return this.col
+	}
 
-    // console.group();
-    // console.log("传入的数据", data);
-    // console.log("生成的数据", nodes);
-    // console.groupEnd();
+	DewNode.prototype.getRow = function(minus) {
+		if (minus) {
+			return this.row + minus
+		}
+		return this.row
+	}
 
-    return nodes;
-  }
+	DewNode.prototype.getPosition = function() {
+		return this.row
+	}
 
-  function _renderContent(ops) {
-    var contentElement = dom.getElementById(ops.content);
+	DewNode.prototype.getName = function() {
+		return this.name
+	}
 
-    if (!contentElement) {
-      throw new Error("dew的容器不是一个正确的元素");
-    }
-    addCss(contentElement, {
-      width: ops.contentWH[0] + "px",
-      height: ops.contentWH[1] + "px",
-      border: "1px solid #EADFFF",
-      background: "#FCFBFF",
-      position: "relative"
-    });
-    return contentElement;
-  }
+	DewNode.prototype.getDirection = function() {
+		return this.direction
+	}
 
-  function _renderNode(ops) {
-    var nodes = ops.nodes;
-    var random = ops.random;
-    var length = nodes.length;
-    var i, j, k, child, bg;
-    for (i = 0; i < length; i++) {
-      var item = nodes[i];
-      var arr = intersection(Object.getOwnPropertyNames(item), random);
+	function _createNode(ops) {
+		var nodes = []
+		var data = ops.data
+		var max = ops.range[1]
+		var dataLength = data.length
+		var i, current, nodeLength, prevNode
 
-      if (arr.length === 0) {
-        throw new Error("当前的节点属性有错误");
-      }
+		for (i = 0; i < dataLength; i++) {
+			current = data[i] // 大,小,小,大
 
-      var num = item[arr[0]];
-      if (arr[0] === random[0]) bg = "#4F62EA";
-      if (arr[0] === random[1]) bg = "#C30E0E";
+			if (i === 0) {
+				nodes.push(new DewNode(current, [1, 1], 'forward'))
+				addPos([1, 1])
+			} else {
+				nodeLength = nodes.length
+				prevNode = nodes[nodeLength - 1]
 
-      //当num大于等于最小值的时候,或者小于等于最大值时候,正常渲染
-      if (num >= item.min && num <= item.max) {
-        for (j = 0; j < num; j++) {
-          child = document.createElement("div");
-          addCss(child, {
-            left: i * 30 + 3 + "px",
-            top: j * 30 + 3 + "px",
-            position: "absolute",
-            width: "24px",
-            height: "24px",
-            background: bg,
-            "border-radius": "90px",
-            display: "inline-block",
-            color: "#fff",
-            "line-height": "24px",
-            "text-align": "center"
-          });
+				if (prevNode.getName() === current) {
+					if (prevNode.getRow() < max) {
+						if (prevNode.getDirection() === 'right') {
+							// 列递增
+							nodes.push(
+								new DewNode(
+									current,
+									[prevNode.getRow(), prevNode.getCol(1)],
+									'right'
+								)
+							)
+							addPos([prevNode.getRow(), prevNode.getCol(1)])
+						} else {
+							// 如果向前已经不可走
+							if (isHasPos([prevNode.getRow(1), prevNode.getCol()])) {
+								// 行递增
+								nodes.push(
+									new DewNode(
+										current,
+										[prevNode.getRow(), prevNode.getCol(1)],
+										'right'
+									)
+								)
+								addPos([prevNode.getRow(), prevNode.getCol(1)])
+							} else {
+								// 行递增
+								nodes.push(
+									new DewNode(
+										current,
+										[prevNode.getRow(1), prevNode.getCol()],
+										'forward'
+									)
+								)
+								addPos([prevNode.getRow(1), prevNode.getCol()])
+							}
+						}
+					} else {
+						// 列递增
+						nodes.push(
+							new DewNode(
+								current,
+								[prevNode.getRow(), prevNode.getCol(1)],
+								'right'
+							)
+						)
+						addPos([prevNode.getRow(), prevNode.getCol(1)])
+					}
+				} else {
+					defaultOptions.list++
 
-          child.innerText = arr[0];
-          ops.contentElement.appendChild(child);
-        }
-      } else {
-        var minus = num - item.max;
-        for (j = 0; j < item.max; j++) {
-          child = document.createElement("div");
-          addCss(child, {
-            left: i * 30 + 3 + "px",
-            top: j * 30 + 3 + "px",
-            position: "absolute",
-            width: "24px",
-            height: "24px",
-            background: bg,
-            "border-radius": "90px",
-            display: "inline-block",
-            color: "#fff",
-            "line-height": "24px",
-            "text-align": "center"
-          });
+					nodes.push(new DewNode(current, [1, defaultOptions.list], 'forward'))
+					addPos([1, defaultOptions.list])
+				}
+			}
+		}
 
-          child.innerText = arr[0];
-          ops.contentElement.appendChild(child);
-        }
+		return nodes
+	}
 
-        for (k = 0; k < minus; k++) {
-          child = document.createElement("div");
-          addCss(child, {
-            left: i * 30 + k * 30 + 3 + "px",
-            top: (j - 1) * 30 + 3 + "px",
-            position: "absolute",
-            width: "24px",
-            height: "24px",
-            background: bg,
-            "border-radius": "90px",
-            display: "inline-block",
-            color: "#fff",
-            "line-height": "24px",
-            "text-align": "center"
-          });
+	function addPos(arr) {
+		defaultOptions.positions.push(arr.toString())
+	}
 
-          child.innerText = arr[0];
-          ops.contentElement.appendChild(child);
-        }
-      }
-    }
-  }
+	function isHasPos(arr) {
+		return defaultOptions.positions.indexOf(arr.toString()) >= 0
+	}
 
-  function _renderColLine(ops) {
-    var index = 1;
-    var run = true;
+	function _renderContent(ops) {
+		var contentElement = dom.getElementById(ops.content)
+		_removeChildNode(contentElement)
 
-    while (run) {
-      var div = document.createElement("div");
-      addCss(div, {
-        left: index * 30 + "px",
-        width: "1px",
-        height: "100%",
-        background: "#EADFFF",
-        display: "inline-block",
-        position: "absolute"
-      });
-      ops.contentElement.appendChild(div);
-      index++;
-      if (index * 30 >= ops.contentWH[0]) {
-        run = false;
-      }
-    }
-  }
+		if (!contentElement) {
+			throw new Error('dew的容器不是一个正确的元素')
+		}
 
-  function _renderRowLine(ops) {
-    var index = 1;
-    var run = true;
+		_addCss(contentElement, {
+			width: ops.contentWH[0] + 'px',
+			height: ops.contentWH[1] + 'px',
+			border: '1px solid #EADFFF',
+			background: '#FCFBFF',
+			position: 'relative'
+		})
 
-    while (run) {
-      var div = document.createElement("div");
-      addCss(div, {
-        top: index * 30 + "px",
-        width: "100%",
-        height: "1px",
-        background: "#EADFFF",
-        display: "inline-block",
-        position: "absolute"
-      });
-      ops.contentElement.appendChild(div);
-      index++;
-      if (index * 30 >= ops.contentWH[1]) {
-        run = false;
-      }
-    }
-  }
+		return contentElement
+	}
 
-  function addCss(context, style) {
-    style = style || {};
-    var names = Object.getOwnPropertyNames(style);
-    var length = names.length;
-    var text = "";
-    var i;
+	function _removeChildNode(element) {
+		while (element.hasChildNodes()) {
+			element.removeChild(element.firstChild)
+		}
+	}
 
-    for (i = 0; i < length; i++) {
-      text += names[i] + ":" + style[names[i]] + ";";
-    }
-    context.setAttribute("style", text);
-  }
+	// 生成节点
+	function _renderNode(ops) {
+		var nodes = ops.nodes
+		var random = ops.random
+		var length = nodes.length
+		var i, child, bg
 
-  function intersection(array) {
-    var result = [];
-    var argLength = arguments.length;
-    var arrLength = array.length;
-    var i;
-    for (i = 0; i < arrLength; i++) {
-      var item = array[i];
-      if (result.indexOf(item) > -1) continue;
-      var j;
-      for (j = 1; j < argLength; j++) {
-        if (arguments[j].indexOf(item) === -1) break;
-      }
-      if (j === argLength) result.push(item);
-    }
-    return result;
-  }
+		// console.group()
+		// console.log('生成的数据', nodes)
+		// console.groupEnd()
 
-  if (typeof module !== "undefined" && typeof exports === "object") {
-    module.exports = createDewChart;
-  } else if (
-    typeof win.define === "function" &&
-    (win.define.amd || win.define.cmd)
-  ) {
-    win.define("createDewChart", [], function () {
-      return createDewChart;
-    });
-  } else {
-    win.createDewChart = createDewChart;
-  }
-})(window, document);
+		for (i = 0; i < length; i++) {
+			var item = nodes[i]
+			var name = item.getName()
+			if (name === random[0]) bg = '#4F62EA'
+			if (name === random[1]) bg = '#C30E0E'
+
+			child = document.createElement('div')
+
+			if (ops.style === 'solid') {
+				_addCss(child, {
+					left: item.getCol(-1) * ops.baseWidth + 3 + 'px',
+					top: item.getRow(-1) * ops.baseWidth + 3 + 'px',
+					position: 'absolute',
+					width: ops.baseWidth - 6 + 'px',
+					height: ops.baseWidth - 6 + 'px',
+					background: bg,
+					'border-radius': '90px',
+					display: 'inline-block',
+					color: '#fff',
+					'line-height': ops.baseWidth - 6 + 'px',
+					'text-align': 'center'
+				})
+			} else {
+				_addCss(child, {
+					left: item.getCol(-1) * ops.baseWidth + 3 + 'px',
+					top: item.getRow(-1) * ops.baseWidth + 3 + 'px',
+					position: 'absolute',
+					width: ops.baseWidth - 6 + 'px',
+					height: ops.baseWidth - 6 + 'px',
+					border: '2px solid ' + bg,
+					'border-radius': '90px',
+					display: 'inline-block',
+					color: '#fff',
+					'line-height': ops.baseWidth - 6 + 'px',
+					'text-align': 'center'
+				})
+			}
+
+			// child.innerText = item.getName()
+			ops.contentElement.appendChild(child)
+		}
+	}
+
+	function _renderColLine(ops) {
+		var index = 1
+		var run = true
+
+		while (run) {
+			var div = document.createElement('div')
+			_addCss(div, {
+				left: index * ops.baseWidth + 'px',
+				width: '1px',
+				height: '100%',
+				background: '#EADFFF',
+				display: 'inline-block',
+				position: 'absolute'
+			})
+			ops.contentElement.appendChild(div)
+			index++
+			if (index * ops.baseWidth >= ops.contentWH[0]) {
+				run = false
+			}
+		}
+	}
+
+	function _renderRowLine(ops) {
+		var index = 1
+		var run = true
+
+		while (run) {
+			var div = document.createElement('div')
+			_addCss(div, {
+				top: index * ops.baseWidth + 'px',
+				width: '100%',
+				height: '1px',
+				background: '#EADFFF',
+				display: 'inline-block',
+				position: 'absolute',
+				left: '0'
+			})
+			ops.contentElement.appendChild(div)
+			index++
+			if (index * ops.baseWidth >= ops.contentWH[1]) {
+				run = false
+			}
+		}
+	}
+
+	function _addCss(context, style) {
+		style = style || {}
+		var names = Object.getOwnPropertyNames(style)
+		var length = names.length
+		var text = ''
+		var i
+
+		for (i = 0; i < length; i++) {
+			text += names[i] + ':' + style[names[i]] + ';'
+		}
+		context.setAttribute('style', text)
+	}
+
+	if (typeof module !== 'undefined' && typeof exports === 'object') {
+		module.exports = createDewChart
+	} else if (
+		typeof win.define === 'function' &&
+		(win.define.amd || win.define.cmd)
+	) {
+		win.define('createDewChart', [], function() {
+			return createDewChart
+		})
+	} else {
+		win.createDewChart = createDewChart
+	}
+})(window, document)
